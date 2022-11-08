f5b4c3f43fe5b42c616516b85d854e378f0f4624
Quentin Aubert <9610898-quentin.aubert@users.noreply.gitlab.com>
2022-11-08 14:07:35 +0100

    feat(api) create, get, update a post

---
+++ src/api/controllers/postController.js
@@ -0,0 +1,73 @@
+const Post = require("../models/postModel");
+
+exports.listAllPosts = (req, res) => {
+  Post.find({}, (error, posts) => {
+    if (error) {
+      res.status(500);
+      console.log(error);
+      res.json({ message: "Erreur serveur." });
+    } else {
+      res.status(200);
+      res.json(posts);
+    }
+  });
+};
+
+exports.createAPost = (req, res) => {
+  let newPost = new Post(req.body);
+
+  newPost.save((error, post) => {
+    if (error) {
+      res.status(401);
+      console.log(error);
+      res.json({ message: "Reqûete invalide." });
+    } else {
+      res.status(201);
+      res.json(post);
+    }
+  });
+};
+
+exports.getAPost = (req, res) => {
+  Post.findById(req.params.post_id, (error, post) => {
+    if (error) {
+      res.status(500);
+      console.log(error);
+      res.json({ message: "Erreur serveur." });
+    } else {
+      res.status(200);
+      res.json(post);
+    }
+  });
+};
+
+exports.updateAPost = (req, res) => {
+  Post.findOneAndUpdate(
+    { _id: req.params.post_id },
+    req.body,
+    { new: true },
+    (error, post) => {
+      if (error) {
+        res.status(500);
+        console.log(error);
+        res.json({ message: "Erreur serveur." });
+      } else {
+        res.status(200);
+        res.json(post);
+      }
+    }
+  );
+};
+
+exports.deleteAPost = (req, res) => {
+  Post.remove({ _id: req.params.post_id }, (error) => {
+    if (error) {
+      res.status(500);
+      console.log(error);
+      res.json({ message: "Erreur serveur." });
+    } else {
+      res.status(200);
+      res.json({ message: "Article supprimé" });
+    }
+  });
+};
--- src/app.js
+++ src/app.js
@@ -1,12 +1,18 @@
-const http = require("http");
+const express = require("express");
 
 const hostname = "0.0.0.0";
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  (res.setHeader = "Content-Type"), "text/html";
-  res.end("kek!");
-});
+const server = express();
+
+const mongoose = require("mongoose");
+// mongoose.connect("mongodb://localhost:27017/apinode"); // Whithout Docker
+mongoose.connect("mongodb://mongo/apinode");
+
+server.use(express.urlencoded());
+server.use(express.json());
+
+const postRoute = require("./api/routes/postRoute");
+postRoute(server);
 
 server.listen(port, hostname);
